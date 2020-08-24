2686edfc89f741509f51b341e41c4877db9ba044
Rugels.Dix <42989816+FormattingClub@users.noreply.github.com>
2020-08-24 19:05:32 +0800

    Add files via upload

---
+++ darknet.sh
@@ -0,0 +1,1 @@
+./darknet detector demo cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=30/1 ! nvvidconv flip-method=0 ! video/x-raw, width=1280, height=720, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink"
